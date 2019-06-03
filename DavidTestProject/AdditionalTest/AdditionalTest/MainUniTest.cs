bc663bb6473795c87430b193b1be263f3c0cb1a3
davednguyen <davednguyen@gmail.com>
2019-06-02 23:26:54 -0700

    add Selenium Test Cases

---
+++ DavidTestProject/AdditionalTest/AdditionalTest/MainUniTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AdditionalTest
+{
+    [TestClass]
+    public class MainUniTest
+    {
+        IWebDriver driver;
+        string homeURL = "http://localhost:59052/";
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver((@"C:\Users\dzzn\Desktop\truonghoc\teamprojectunittest\DavidTestProject\AdditionalTest\AdditionalTest\bin\Debug\"), new ChromeOptions());
+        }
+
+        [TestMethod]
+        public void LoadPageAndSelectUser()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(homeURL);           
+            IWebElement MikeAvarta = driver.FindElement(By.Id("e219a7f5-0b9e-48fa-a1e0-8b4bc02a20d9"));
+            int milliseconds = 3000;
+            Thread.Sleep(milliseconds);
+            MikeAvarta.Click();
+            IWebElement avatarButton = driver.FindElement(By.Id("avatarLinkPortalIndex"));
+            IWebElement imgSource = avatarButton.FindElement(By.CssSelector("#avatarLinkPortalIndex > img"));
+            string imageSrcValue = imgSource.GetAttribute("src");
+            Thread.Sleep(milliseconds);
+            avatarButton.Click();
+
+        }
+
+        [TestMethod]
+        public void LoadPageAndSelectUserGetAvataImageValue()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(homeURL);
+            IWebElement MikeAvarta = driver.FindElement(By.Id("e219a7f5-0b9e-48fa-a1e0-8b4bc02a20d9"));
+            int milliseconds = 3000;
+            Thread.Sleep(milliseconds);
+            MikeAvarta.Click();
+            IWebElement avatarButton = driver.FindElement(By.Id("avatarLinkPortalIndex"));
+            IWebElement imgSource = avatarButton.FindElement(By.CssSelector("#avatarLinkPortalIndex > img"));
+            string imageSrcValue = imgSource.GetAttribute("src");
+            string expectedImageSrc = "http://localhost:59052/Content/img/Avatars_dark.png";
+            driver.Close();
+            Assert.AreEqual(expectedImageSrc, imageSrcValue);         
+        }
+    }
+}
---
+++ DavidTestProject/AdditionalTest/AdditionalTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdditionalTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdditionalTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f199ed3-a238-4aa7-8f6d-502fb502de88")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
